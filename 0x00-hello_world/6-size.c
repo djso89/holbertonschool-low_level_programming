4c217953f125e10039ae7f7f7c3ed55087a9d20f
djso89 <danielso89@gmail.com>
2020-02-05 17:24:18 -0800

    took out l from %ld

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %ld byte(s)\n", sizeof(a));
-printf("Size of an int: %ld byte(s)\n", sizeof(b));
-printf("Size of a long int: %ld bytes(s)\n", sizeof(c));
-printf("Size of a long long int: %ld bytes(s)\n", sizeof(d));
-printf("Size of a float: %ld bytes(s)\n", sizeof(e));
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("Size of a long int: %d bytes(s)\n", sizeof(c));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(d));
+printf("Size of a float: %d bytes(s)\n", sizeof(e));
 return (0);
 }
