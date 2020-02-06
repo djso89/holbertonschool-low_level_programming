76065bcbe6963a2558532a1f09955f1efb85651d
djso89 <danielso89@gmail.com>
2020-02-05 16:17:43 -0800

    fixed betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /*
- * This is the preferred style for multi-line
- * comments in C source code.
- * Please use it consistently.
- * Description:  A column of asterisks on the left side,
- * with beginning and ending almost-blank lines.
- */
+* main - Entry Point
+* Description:  A column of asterisks on the left side,
+* with beginning and ending almost-blank lines.
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
