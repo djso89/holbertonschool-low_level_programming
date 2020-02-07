14fba8a1d188bbff3c4d368ad6a4cf2bcef5735d
djso89 <danielso89@gmail.com>
2020-02-06 23:12:48 -0800

    first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry Point
+* Description: print a-z in reverse
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+  int i;
+
+for (i = 0; i <=  9; i++)
+{
+  
+putchar('0'+i);
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+else
+{
+  putchar('\n');
+}
+  
+}
+ 
+return (0);
+}
+  
