4310fd25d7b09a972901c5776238e82afbbd6fe2
djso89 <danielso89@gmail.com>
2020-02-11 10:45:19 -0800

    first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* add - function
+* Description: print the last digit
+* @a: takes the integer number
+* @b: number to add
+* Return: Value of sum
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
