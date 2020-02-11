3dfe5edfbee6fc8ac54964f4e244053455ccb4d9
djso89 <danielso89@gmail.com>
2020-02-11 09:17:06 -0800

    first commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* jack_bauer - function
+* Description: print out every minute of the day
+* Return: number with no signs
+*/
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
+		}
+	}
+}
