43291304def7d21d2d618db2ca06617d649aadc1
djso89 <danielso89@gmail.com>
2020-02-11 06:35:19 -0800

    add new function prototype

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+* print_last_digit - function
+* Description: print the last digit
+* @n: takes the integer number
+* Return: Value of last digit
+*/
+int print_last_digit(int n)
+{
+	int r;
+	r = n % 10;
+	if (r < 0)
+	{
+		r = -1 * r;
+		_putchar('0' + r);
+		return (r);
+	}
+	else
+	{
+		_putchar('0' + r);
+		return (n % 10);
+	}
+}
