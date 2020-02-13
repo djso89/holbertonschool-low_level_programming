fe2b4db3a663864c2face8621edae8cbf50c4c52
djso89 <danielso89@gmail.com>
2020-02-13 12:49:42 -0800

    first commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,5 +6,27 @@
 */
 int main(void)
 {
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 3 != 0 && num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("\n");
 	return (0);
 }
