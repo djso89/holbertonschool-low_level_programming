a687b1cc0020b12f5176278104cacb4f40db47e8
djso89 <danielso89@gmail.com>
2020-02-13 10:24:28 -0800

    added more prototype

--- 0x04-more_function_nested_loops/holberton.h
+++ 0x04-more_function_nested_loops/holberton.h
@@ -1,6 +1,12 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 
+int _putchar(char);
 int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
 
 #endif
