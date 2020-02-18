b73f59365ef5db98a952112e86b3b389f26c6c85
djso89 <danielso89@gmail.com>
2020-02-18 11:27:57 -0800

    first commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+* puts_half - function
+* Description: print the second half of string
+* @str: char pointer
+* Return: nothing
+*/
+void puts_half(char *str)
+{
+	int numchar;
+	int i;
+
+	numchar = 0;
+	while (*(str + numchar) != '\0')
+	{
+		numchar++;
+	}
+	if (numchar % 2 == 0)
+	{
+		for (i = numchar / 2; i < numchar; i++)
+		{
+			_putchar(*(str + i));
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = (numchar - 1) / 2; i < numchar; i++)
+		{
+			_putchar(*(str + i));
+		}
+		_putchar('\n');
+	}
+}
