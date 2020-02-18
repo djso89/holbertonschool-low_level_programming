155c0699f7ac57e32fcc814408c8afed1f23eea5
djso89 <danielso89@gmail.com>
2020-02-18 09:37:12 -0800

    first commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* rev_string - function
+* Description: reverse the string
+* @s: char pointer
+* Return: nothing
+*/
+void rev_string(char *s)
+{
+	int numchar;
+	char *start;
+	char *end;
+	char temp;
+
+	start = s;
+	/*get the string length*/
+	while (*(s + numchar) != '\0')
+	{
+		numchar++;
+	}
+	end = start + numchar - 1;
+	while (end > start)
+	{
+		temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
+}
