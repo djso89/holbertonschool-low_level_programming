7445d97d4a22c9e13e4d5f79bf7fc124a34ced68
djso89 <danielso89@gmail.com>
2020-02-17 18:33:41 -0800

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* reset_to_98 - function
+* Description: print square using #
+* @n: integer pointer n to be reset
+* Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
