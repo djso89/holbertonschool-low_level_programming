ae2055c125f3b2b0b04d79e54c946205b03868e9
djso89 <danielso89@gmail.com>
2020-02-20 14:26:21 -0800

    fixed betty format

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,10 @@
 int _strcmp(char *s1, char *s2)
 {
 	/*run the loop as long as s1 is not \0*/
-	while(*s1)
+	while (*s1)
 	{
 		/*when s1 and s2 are not same, then break out of loop*/
-		if(*s1 != *s2)
+		if (*s1 != *s2)
 		{
 			break;
 		}
