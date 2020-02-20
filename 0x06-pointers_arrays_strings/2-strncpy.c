a298e9736666c68b1d76403fd25ef8e1cf9ac14b
djso89 <danielso89@gmail.com>
2020-02-20 13:51:11 -0800

    added more conditionals

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,14 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	while (i <= n)
+	while (i < n && *(src + i))
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
-	if (i < n)
+	while (i < n)
 	{
-		*(dest + i) = '\n';
+		*(dest + i) = '\0';
+		i++;
 	}
 	return (dest);
 }
