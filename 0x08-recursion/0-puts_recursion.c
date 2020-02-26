3237eb4fa60ca45e38b3bcee52631b3ff7709300
djso89 <danielso89@gmail.com>
2020-02-26 09:14:55 -0800

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* _puts_recursion - function
+* Description: a function that prints the string and followed by newline
+* @s: string to print
+* Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s)
+	{
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
