4f1c069f08d39e7c8ac956ae7480970ab00016c2
djso89 <danielso89@gmail.com>
2020-02-26 14:48:20 -0800

    first commit

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,25 @@
+/**
+* wildcmp - function
+* Description: a function that compares two strings and determine if the
+* string is considered as identical
+* @s1: target string
+* @s2: string to compared with. this string can contain special character
+* "*"
+* Return: 1 if string is considered identical, otherwise 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	return (0);
+}
