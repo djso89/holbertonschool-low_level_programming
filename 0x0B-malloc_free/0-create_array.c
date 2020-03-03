e699a3debcd426a09b8af621beb986c85a42b049
djso89 <danielso89@gmail.com>
2020-03-03 12:21:09 -0800

    fixed if statement

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,15 +14,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	array = malloc(size * sizeof(char));
-	if (size == 0)
+	if (array == NULL || size == 0)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
-
 	return (array);
 }
