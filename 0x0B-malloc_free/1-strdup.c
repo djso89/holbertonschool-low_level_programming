d905a009fc82149e764abd3de9f78cc9eed6fbd5
djso89 <danielso89@gmail.com>
2020-03-03 11:57:17 -0800

    added if statement to check if pointer is null

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* _strdup - function
+* Description: a function that returns a pointer to a newly allocated space in
+* memory, which contains a copy of the string given as a parameter
+* @str: specific char to initialize with
+* Return: pointer to the array, or null if fails
+*/
+char *_strdup(char *str)
+{
+	char *s;
+	int len;
+	int i;
+
+	len = 0;
+	while(*(str + len))
+	{
+		len++;
+	}
+
+	s = malloc(sizeof(char) * len);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+}
