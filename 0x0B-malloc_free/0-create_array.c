34a3577ed6d8a6f80580c17a9b705127e1e90a32
djso89 <danielso89@gmail.com>
2020-03-03 12:10:55 -0800

    got rid of typecast

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
-	array = (char *)malloc(size * sizeof(char));
+	array = malloc(size * sizeof(char));
 	if (size == 0)
 	{
 		return (NULL);
