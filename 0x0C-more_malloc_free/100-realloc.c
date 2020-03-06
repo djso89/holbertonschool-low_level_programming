5b3b8319225868396f9e40788a43f08a8821badc
djso89 <danielso89@gmail.com>
2020-03-06 13:00:40 -0800

    first commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+* _realloc - function
+* Description: a function that reallocates a memory block using malloc and free
+* @ptr:the pointer to re-allocate
+* @old_size: size of the allocated space for ptr
+* @new_size: new size in bytes of the new memory block
+* Return: returns a pointer to the newly created array.
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ret_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		ret_ptr = malloc(new_size);
+		if (ret_ptr == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < old_size; i++)
+		{
+			ret_ptr[i] = *((char *)ptr + i);
+		}
+		free(ptr);
+	}
+	else
+	{
+		ret_ptr = malloc(new_size);
+		if (ret_ptr == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < new_size; i++)
+		{
+			ret_ptr[i] = *((char *)ptr + i);
+		}
+		free(ptr);
+	}
+	return (ret_ptr);
+}
