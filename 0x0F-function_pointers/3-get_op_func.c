1cb77b5cd83ad8cff42ed82a4d69c496fd1fc10f
djso89 <danielso89@gmail.com>
2020-03-11 20:46:35 -0700

    got rid of for loop and replaced with while

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ * get_op_func - function
+ * Description: a fucntion that selects the operation
+ * @s: string to compare for operator
+ * Return: a function that points to corresponding operation
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
