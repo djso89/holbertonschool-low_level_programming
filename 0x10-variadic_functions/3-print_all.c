701bab4db75d5de30904b919e7e1c999b3018fa5
djso89 <danielso89@gmail.com>
2020-03-14 16:12:31 -0700

    realized that i didn't print nil properly

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,62 @@
 #include "variadic_functions.h"
 /**
+ * is_valid - function
+ * Description: a function that checks if format string is valid
+ * @fc: format character
+ * Return: 1 if fc is c,s,f,i. Otherwise, 0
+ */
+int is_valid(const char fc)
+{
+	switch (fc)
+	{
+	case 'c':
+		return (1);
+	case 'i':
+		return (1);
+	case 'f':
+		return (1);
+	case 's':
+		return (1);
+	}
+	return (0);
+}
+/**
  * print_all - function
  * @format: list of types of arguments passed to function
  * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
-	int i, flag;
+	unsigned int i;
 	va_list items;
 	char *str;
 
 	i = 0;
-	while (format)
+	va_start(items, format);
+	while (format && format[i])
 	{
-		va_start(items, format);
-		while (format[i])
+		switch (format[i])
 		{
-			flag = 1;
-			switch (format[i])
-			{
-			case 'c':
-				printf("%c", va_arg(items, int));
-				break;
-			case 'i':
-				printf("%i", va_arg(items, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(items, double));
-				break;
-			case 's':
-				str = va_arg(items, char *);
-				if (!str)
-					printf("(nil)");
-				printf("%s", str);
-				break;
-			default:
-				flag = 0;
-				break;
-			}
-			if (flag && format[i + 1] != '\0')
-				printf(", ");
-			i++;
+		case 'c':
+			printf("%c", va_arg(items, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(items, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(items, double));
+			break;
+		case 's':
+			str = va_arg(items, char *);
+			if (!str)
+				str = "(nil)";
+			printf("%s", str);
+			break;
 		}
-		va_end(items);
-		break;
+		if (is_valid(format[i]) && format[i + 1] != '\0')
+			printf(", ");
+		i++;
 	}
 	printf("\n");
+	va_end(items);
 }
