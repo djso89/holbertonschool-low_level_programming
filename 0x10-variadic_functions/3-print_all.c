0102157320b7ca3c4eaff0d041f3376f25774f9b
djso89 <danielso89@gmail.com>
2020-03-14 15:12:35 -0700

    got rid of isvalid

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,26 +1,5 @@
 #include "variadic_functions.h"
 /**
- * is_valid - function
- * Description: a function that checks if format string is valid
- * @fc: format character
- * Return: 1 if fc is c,s,f,i. Otherwise, 0
- */
-int is_valid(const char fc)
-{
-	switch (fc)
-	{
-	case 'c':
-		return (1);
-	case 'i':
-		return (1);
-	case 'f':
-		return (1);
-	case 's':
-		return (1);
-	}
-	return (0);
-}
-/**
  * print_all - function
  * @format: list of types of arguments passed to function
  * Return: nothing
@@ -53,7 +32,8 @@ void print_all(const char * const format, ...)
 			printf("%s", str);
 			break;
 		}
-		if (is_valid(format[i]) && format[i + 1])
+		if ((format[i] == 'c' || format[i] == 's' || format[i] == 'i'
+		     || format[i] == 'f') && format[i + 1] != '\0')
 			printf(", ");
 		i++;
 	}
