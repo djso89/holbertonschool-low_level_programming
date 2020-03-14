bc88bda363ab4b96f0096a14c0ec5ecd133b3090
djso89 <danielso89@gmail.com>
2020-03-14 14:53:42 -0700

    got rid of if

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,15 +13,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(num_list, n);
 
-	if (separator) /* if seperator is not null*/
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(num_list, int)); /*print number*/
-			if (i != n - 1) /* check if it reaches last parameter*/
-				printf("%s", separator);
-		}
-		va_end(num_list);
-		printf("\n");
+		printf("%d", va_arg(num_list, int)); /*print number*/
+		if (i != n - 1 && separator)
+			printf("%s", separator);
 	}
+	va_end(num_list);
+	printf("\n");
 }
