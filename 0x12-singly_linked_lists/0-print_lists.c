c04a8be2a3cf5fbab6fed3918716e6788f07c35d
djso89 <danielso89@gmail.com>
2020-03-19 14:58:26 -0700

    first commit

---
+++ 0x12-singly_linked_lists/0-print_lists.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - function
+ * Description: a function that prints all the elements of a list_t list
+ * @h: head
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int nodes;
+
+	for (nodes = 0; h; nodes++)
+	{
+		if (h->str)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+	}
+	return (nodes);
+}
