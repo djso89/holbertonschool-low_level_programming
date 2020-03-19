9fbe2d3cd244129a78f721ea790c8a5948493899
djso89 <danielso89@gmail.com>
2020-03-19 15:11:03 -0700

    added h= h->head

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,12 +13,13 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str)
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		else
 		{
 			printf("[0] (nil)\n");
 		}
+		h = h->next;
 	}
 	return (nodes);
 }
