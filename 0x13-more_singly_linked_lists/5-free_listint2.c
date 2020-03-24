7e40b5b23936ba7ff5d2f31fb9c76c08a6909b37
djso89 <danielso89@gmail.com>
2020-03-24 15:21:17 -0700

    first commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - function
+ * Description: a function that frees a listint_t list and sets head to NULL
+ * @head: start pointer
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *delete;
+
+	while (*head)
+	{
+		delete = (*head)->next;
+		free(*head);
+		*head = delete;
+	}
+	*head = NULL;
+}
