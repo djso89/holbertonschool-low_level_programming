bbb700adbff3a04e2d9e65d1631c3d6d4c11192f
djso89 <danielso89@gmail.com>
2020-03-26 20:31:23 -0700

    moved _putchar to separate file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,16 +1,4 @@
 #include "holberton.h"
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On Success 1
- * On error, -1 is returned, and errno is set appropriately
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 /**
  * print_binary - function that prints the binary representation of
  * number.
