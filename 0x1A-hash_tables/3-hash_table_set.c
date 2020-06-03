8ceb290f887e03f77f03b3c333b7956f56baa9ee
djso89 <danielso89@gmail.com>
2020-06-03 01:28:57 -0700

    modified the function to fix task 6

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,6 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 /**
+ * add_node - allocates a new node
+ * @key: the key
+ * @value: the value
+ * Return: ptr to the node or NULL
+ */
+hash_node_t *add_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+	node->key = strdup(key);
+	if (!node->key)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+
+	return (node);
+}
+/**
  * hash_table_set - function that adds an element to the table
  * @ht: the hash table to be updated
  * @key: key in string
@@ -10,26 +39,32 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int h;
-	hash_node_t *add;
+	hash_node_t *new = NULL, *head = NULL;
+	unsigned long int index;
+	char *new_value;
 
-	if (!key || !ht)
+	if (!ht || !key)
 		return (0);
 
-	h = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[h] && strcmp(ht->array[h]->key, key) == 0)
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+	while (head)
 	{
-		ht->array[h]->value = strdup(value);
-		return (1);
+		if (!strcmp(key, head->key))
+		{
+			new_value = strdup(value);
+			if (!new_value)
+				return (0);
+			free(head->value);
+			head->value = new_value;
+			return (1);
+		}
+		head = head->next;
 	}
-	add = malloc(sizeof(hash_node_t));
-	if (!add)
+	new = add_node(key, value);
+	if (!new)
 		return (0);
-	add->key = strdup(key);
-	add->value = strdup(value);
-	add->next = ht->array[h];
-	ht->array[h] = add;
-
+	new->next = ht->array[index];
+	ht->array[index] = new;
 	return (1);
 }
